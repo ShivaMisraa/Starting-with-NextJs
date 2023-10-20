e10b56198b636d9bea7ec833e0fdf3632865356e
ShivaMisraa <shivam18557@gmail.com>
2023-10-20 23:19:47 +0530

    Added nested route

---
+++ src/pages/aboutUs/developer.js
@@ -0,0 +1,12 @@
+// our-domain.com/AboutUsPage/developerPage
+
+const DeveloperPage = () => {
+  return (
+    <>
+      <h1>This is Developer Page</h1>
+      <hi>This is Shivam Mishra</hi>
+    </>
+  );
+};
+
+export default DeveloperPage;
---
+++ src/pages/news/something-imp.js
@@ -0,0 +1,10 @@
+// our-domain.com/news/DetailsPage
+
+
+const DetailsPage = () => {
+    return (
+      <h1>This is Details Page</h1>
+    )
+  }
+  
+  export default DetailsPage;
